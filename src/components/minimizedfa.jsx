514d11addf2b92bb04777484d9f1fd86b52f0543
J_Ghannam <97244554+JGhannam@users.noreply.github.com>
2023-11-12 13:49:09 +0200

    Update minimizedfa.jsx

--- src/components/minimizedfa.jsx
+++ src/components/minimizedfa.jsx
@@ -1,95 +1,76 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import DFAVisualization from './dfavixgraph';
 
-class MinimizedDFA extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      minimizedGraph: this.minimizeDFA(props.graph),
-    };
-  }
+function MinimizeDFA({ graph }) {
+  const [minimizedGraph, setMinimizedGraph] = useState(null);
 
-  getNextState(state, symbol, graph) {
-    for (let transition of graph.transitions) {
-      if (transition.from === state && transition.symbol === symbol) {
-        return transition.to;
-      }
-    }
-    return null;
-  }
+  useEffect(() => {
+    minimizeDFA();
+  }, [graph]);
 
-  removeUnreachableStates(graph) {
-    let reachableStates = new Set([graph.startState]);
-    let newStates = new Set([graph.startState]);
-
-    while (newStates.size > 0) {
-      let currentStates = new Set(newStates);
-      newStates.clear();
-
-      for (let state of currentStates) {
-        for (let transition of graph.transitions) {
-          if (transition.from === state && !reachableStates.has(transition.to)) {
-            reachableStates.add(transition.to);
-            newStates.add(transition.to);
+  function minimizeDFA() {
+    const A = new Set([graph.startState]);
+    const B = new Set();
+  
+    while (A.size > 0) {
+      const q = A.values().next().value;
+      A.delete(q);
+  
+      for (const a of [0, 1]) {
+        // Check if the transition is found
+        const transition = graph.transitions.find(t => t.source === q && t.input === a);
+        if (transition) {
+          const nextState = transition.target;
+          
+          // Add nextState to A or B based on its presence in A or B
+          if (A.has(nextState)) {
+            A.add(nextState);
+          }
+          if (B.has(nextState)) {
+            B.add(nextState);
           }
         }
       }
     }
-
-    graph.states = graph.states.filter(state => reachableStates.has(state));
-    graph.transitions = graph.transitions.filter(transition => reachableStates.has(transition.from));
-  }
-
-  partitionStates(graph) {
-    let finalStates = new Set(graph.finalStates);
-    let nonFinalStates = new Set(graph.states.filter(state => !finalStates.has(state)));
-    return [Array.from(finalStates), Array.from(nonFinalStates)];
-  }
-
-  mergeIndistinguishableStates(graph, partitions) {
-    let newPartitions = [];
-    let symbols = [...new Set(graph.transitions.map(transition => transition.symbol))];
-
-    for (let partition of partitions) {
-      let groups = new Map();
-      for (let state of partition) {
-        let key = symbols.map(symbol => this.getNextState(state, symbol, graph)).join(",");
-        let group = groups.get(key) || [];
-        group.push(state);
-        groups.set(key, group);
+  
+    const minimizedStates = [...A, ...B];
+    const minimizedTransitions = [];
+    for (const state of minimizedStates) {
+      for (const input of [0, 1]) {
+        // Check if the transition is found
+        const transition = graph.transitions.find(t => t.source === state && t.input === input);
+        if (transition) {
+          const nextState = transition.target;
+          const minimizedNextState = minimizedStates.indexOf(nextState);
+          minimizedTransitions.push({
+            source: state,
+            input,
+            target: minimizedNextState,
+          });
+        }
       }
-      newPartitions.push(...groups.values());
     }
+  
+    setMinimizedGraph({
+      startState: graph.startState,
+      finalStates: graph.finalStates.filter(s => A.has(s)),
+      states: minimizedStates,
+      transitions: minimizedTransitions,
+    });
+  }
+  
 
-    return newPartitions;
+  if (minimizedGraph) {
+    console.log(minimizedGraph);
   }
 
-  minimizeDFA(graph) {
-    this.removeUnreachableStates(graph);
-    let partitions = this.partitionStates(graph);
-
-    let oldPartitions;
-    do {
-      oldPartitions = partitions;
-      partitions = this.mergeIndistinguishableStates(graph, partitions);
-    } while (partitions.length !== oldPartitions.length);
-
-    // Update the graph with the new states and transitions
-    graph.states = partitions.map(partition => partition.join(","));
-    graph.transitions = graph.transitions.map(transition => ({
-      from: partitions.find(partition => partition.includes(transition.from)).join(","),
-      to: partitions.find(partition => partition.includes(transition.to)).join(","),
-      symbol: transition.symbol
-    }));
-    graph.startState = partitions.find(partition => partition.includes(graph.startState)).join(",");
-    graph.finalStates = graph.finalStates.map(state => partitions.find(partition => partition.includes(state)).join(","));
-
-    return graph;
-  }
-
-  render() {
-    return <DFAVisualization graph={this.state.minimizedGraph} />;
-  }
+  return (
+    <div>
+      <DFAVisualization 
+        graph={minimizedGraph}
+      />
+    </div>
+  );
 }
 
-export default MinimizedDFA;
+export default MinimizeDFA;
